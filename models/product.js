8c60c997c6ddbbe3269cd9c4619004ff1f336cba
Shane Harper <cdtharper@gmail.com>
2020-03-29 22:45:03 -0400

    added edit product feature

--- models/product.js
+++ models/product.js
@@ -12,17 +12,14 @@ const getProductsFromFile = cb => {
     if (err) {
       cb([]);
     } else {
-      if (JSON.parse(fileContent)) {
-        cb(JSON.parse(fileContent));
-      } else {
-        cb([]);
-      }
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -30,12 +27,23 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+          this.id = Math.random().toString();
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), err => {
+            console.log(err);
+          });
+      }
     });
   }
 
@@ -44,7 +52,7 @@ module.exports = class Product {
   }
 
   static findById(id, cb) {
-    getProductsFromFile(products=> {
+    getProductsFromFile(products => {
       const product = products.find(p => p.id === id);
       cb(product);
     });
