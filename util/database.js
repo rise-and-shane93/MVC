f57b8f0a852348d1ab75f11a728667517a9a4878
Shane Harper <cdtharper@gmail.com>
2020-04-06 08:52:50 -0400

    add sequelize sync

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'password'
+const sequalize = new Sequelize('node-complete', 'root', 'password', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequalize;
